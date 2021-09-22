61c69e49e130ec0808f10c227ae1594ed8b32dc4
Nidhi <singhnidhi.freelancer@gmail.com>
2021-09-22 17:01:21 +0530

    Change structure from class based to function based

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
-export class NavBar extends Component {
-    render() {
+const NavBar = (props) => {
         return (
             <div>
                 <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -33,7 +32,7 @@ export class NavBar extends Component {
                 </nav>
             </div>
         )
-    }
+    
 }
 
 export default NavBar
