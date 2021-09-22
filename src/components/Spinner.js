65ea0b635b7c54b1ee0672b07ceeca98a4bd6444
Nidhi <singhnidhi.freelancer@gmail.com>
2021-09-22 17:06:53 +0530

    Change structure from class based to function based

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import loading from './loading.gif'
 
-export default class spinner extends Component {
-    render() {
+const Spinner = () => {
         return (
             <div className="text-center">
                 <img className="my-3" src ={loading} alt="Loading GIF"/>
             </div>
         )
-    }
+  
 }
+export default Spinner
